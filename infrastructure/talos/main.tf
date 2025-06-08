d0392f4d01ba21f6f8bbf61c81b9fb81b79cfffa
Nick Conway <nick@conway.dev>
2025-06-08 14:30:27 -0400

    update terraform variables

--- infrastructure/talos/main.tf
+++ infrastructure/talos/main.tf
@@ -8,9 +8,9 @@ terraform {
 }
 
 provider "proxmox" {
-  pm_api_url          = "${var.pm_api_url}"
+  pm_api_url          = "${var.pm_api_host}/api2/json"
   pm_user             = "${var.pm_user}"
-  pm_api_token_id     = "${var.pm_api_token_id}"
+  pm_api_token_id     = "${var.pm_user}!${var.pm_api_token_id}"
   pm_api_token_secret = "${var.pm_api_token_secret}"
 }
 
